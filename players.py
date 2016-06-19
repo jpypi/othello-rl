0a9cf9058e311b0c656ef42f29f88f0e053f318f
James Jenkins <applesmacs@yahoo.com>
2016-06-18 21:36:52 -0400

    NN will actually make moves

--- players.py
+++ players.py
@@ -12,20 +12,33 @@ class RLPlayer:
         # We should anneal this
         self.epsilon = 0.8
 
+
     def play(self, place_func, board_state):
         input_state = board_state.reshape((64, 1))
         out = self.policy_net.getOutput(input_state)
 
+        made_move = False
+
         # epsilon greedy
-        if True: #np.random.random() < self.epsilon:
+        if np.random.random() < self.epsilon:
             # Pick random move
-            made_move = False
             while not made_move:
                 pos = np.random.randint(0,8), np.random.randint(0,8)
                 made_move = place_func(*pos)
         else:
-            # Pick best move from
-            pass
+            print("NN playing!")
+            # Sort the possible moves lowest to highest desire
+            positions = [(v,i) for i,v in enumerate(out)]
+            positions.sort(key = lambda x: x[0], reverse = True)
+            #scalar_play_point = np.argmax(out)
+
+            print(positions)
+            while not made_move:
+                # Grab next desired move point
+                scalar_play_point = positions.pop()[1]
+                # Convert the scalar to a 2D coordinate to play on the board
+                pos = scalar_play_point // 8, scalar_play_point % 8
+                made_move = place_func(*pos)
 
     def updateWeights(self):
         pass

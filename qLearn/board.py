bcb46a639f637ddfedac333a50c1d3c2abdf47fe
Ian Ramsay <ianalexr@yahoo.com>
2016-06-20 20:43:51 -0400

    Q Learning algorithm implementation

---
+++ qLearn/board.py
@@ -0,0 +1,338 @@
+import numpy as np
+from reinforce import Player
+
+import math
+#from termcolor import colored
+from colorama import Fore, Back, Style
+from colorama import init
+init(autoreset=True)
+
+board = None
+
+def getRow(row):
+    ret = ""
+    for i in np.nditer(row):
+        ret += getItem(i)
+    return ret
+
+def getItem(item):
+    if item == 1 :
+        return Fore.WHITE + "|" + Fore.BLACK + "O"
+    elif  item == 0 :
+        return Fore.WHITE + "| "
+    else :
+        return Fore.WHITE + "|" + Fore.WHITE + "O"
+
+class Board(object):
+    #player colors
+    BLACK = 1 #player 1
+    WHITE = -1 #player 2
+
+    def __init__(self, size=8):
+        size = int(size)
+        if size % 2 != 0:
+            size += 1
+        self.size = size
+        self.newGame()
+        return
+
+    def newGame(self):
+        """
+        Load board with init conditions
+        And sync virtual board
+        """
+        self.board = np.array([ [0]*self.size ] * self.size, dtype=int)
+        mL = int(self.board.shape[0]/2 - 1)
+        mR = int(self.board.shape[0]/2)
+        self.board[mL][mL] = 1
+        self.board[mR][mR] = 1
+        self.board[mR][mL] = -1
+        self.board[mL][mR] = -1
+
+        self.nextTurn = self.BLACK
+        self.syncVirtualBoard()
+        self.gameOver = False
+
+        return
+
+    def loadFromState(self, t, state):
+        if t == 'live':
+            tBoard = self.board
+        else:
+            tBoard = self.virtualBoard
+
+        for x in range(self.size):
+            for y in range(self.size):
+                tBoard[x][y] = state[x * self.size + y]
+
+    def syncVirtualBoard(self):
+        """
+        Syncronize virtual and current board
+        """
+        self.virtualBoard = np.copy(self.board)
+        self.virtualNextTurn = self.nextTurn
+        return
+
+    def getBoard(self):
+        return self.board
+    def validateBoard(self, t='live'):
+        '''
+        Determines if players can still make moves.
+        If a player cannot move, their turn is automatically skipped.
+        If neither player can move, the game has ended.
+        Returns False if the game continues, -1 or 1 if the game has ended.
+        '''
+        if self.isGameOver(t):
+            print("Game Over")
+            wOwn = self.getOwnership(self.WHITE)
+            bOwn = self.getOwnership(self.BLACK)
+            if wOwn > bOwn:
+                print("White wins with", wOwn, "territory to black's", bOwn, ".")
+                return self.WHITE 
+            else:
+                print("Black wins with", bOwn, "territory to white's", wOwn, ".")
+                return self.BLACK 
+
+        nt = self.nextTurn if t == 'live' else self.virtualNextTurn
+        if not self.canMove(t, nt): 
+            if nt == self.BLACK:
+                print("Black cannot move, turn passed.")
+                if t == 'live':
+                    self.nextTurn = self.WHITE
+                else:
+                    self.virtualNextTurn = self.WHITE
+            else:
+                print("White cannot move, turn passed.")
+                if t == 'live':
+                    self.nextTurn = self.BLACK
+                else:
+                    self.virtualNextTurn = self.BLACK
+        return False 
+ 
+
+    def isGameOver(self, t):
+        for x  in range(self.size):
+            for y in range(self.size):
+                valid = self.isValidMove(t, self.BLACK, x, y) or self.isValidMove(t, self.WHITE, x, y)
+                if valid:
+                    return False
+        return True 
+
+    def canMove(self, t, color):
+        for x in range(self.size):
+            for y in range(self.size):
+                valid = self.isValidMove(t, color, x, y)
+                if valid:
+                    return True
+        return False
+    
+    def updateBoard(self, t, tile, row, col):
+        """
+        @param string t
+            either 'virtual' or 'live'
+        @param int tile
+            either 1 or -1
+                1 for player 1 (black)
+                -1 for player 2 (white)
+        @param int row
+            0-7 which row
+        @param int col
+            0-7 which col
+        @return bool
+            true if valid
+            false if invalid move - doesn't update board
+        """
+        board = self.board if t == 'live' else self.virtualBoard
+        result = self.isValidMove(board, tile, row, col)
+        if result != False:
+            if t == 'live' :
+                self.nextTurn = self.BLACK if self.nextTurn != self.BLACK else self.WHITE
+            else:
+                self.virtualNextTurn = self.BLACK if self.virtualNextTurn != self.BLACK else self.WHITE
+
+            board[row][col] = tile
+            for row in result:
+                board[ row[0] ][ row[1] ] = tile
+            if ( t == 'live'):
+                self.syncVirtualBoard()
+
+            return True
+        else:
+            return False
+
+    def getOwnership(self, tile):
+        flatBoard = self.board.flatten()
+        owned = 0
+        total = 0
+        for t in flatBoard:
+            if t != 0:
+                total += 1
+            if t == tile:
+                owned += 1
+        return owned/total
+
+    def printBoard(self, t='live'):
+        """
+        Print board to terminal for debugging
+        @param string type 'virtual' or 'live'
+        """
+        board = self.board if t == 'live' else self.virtualBoard
+
+        print(Back.GREEN +              "\t      BOARD      ")
+        
+        # Print column titles 
+        head = Back.GREEN + Fore.WHITE + "\t "
+        for i in range(self.board.shape[0]):
+            head += '|' + str(i)
+        print(head)
+      
+        # Print rows 
+        for i in range(self.board.shape[0]):
+            print(Back.GREEN + Fore.WHITE + "\t" + str(i) + getRow(board[i]))
+        print(Style.RESET_ALL)
+
+        return
+
+    def isOnBoard(self, x, y):
+        """
+        Returns True if the coordinates are located on the board.
+        """
+        return x >= 0 and x < self.board.shape[0] and y >= 0 and y < self.board.shape[0] 
+
+    def isValidMove(self, t, tile, xstart, ystart):
+        """
+        From https://inventwithpython.com/reversi.py
+        @param String t 'live' or 'virtual'
+        @param int tile
+            self.BLACK or self.WHITE
+        @param int xstart
+        @param int ystart
+        Returns False if the player's move on space xstart, ystart is invalid.
+        If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
+        """
+        board = self.board if str(t) == 'live' else self.virtualBoard
+        if not self.isOnBoard(xstart, ystart) or board[xstart][ystart] != 0:
+            return False
+
+        # temporarily set the tile on the board.
+        board[xstart][ystart] = tile
+
+        otherTile = tile * -1
+
+        tilesToFlip = []
+        # loop through all directions around flipped tile
+        for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+            x, y = xstart, ystart
+            x += xdirection # first step in the direction
+            y += ydirection # first step in the direction
+            if self.isOnBoard(x, y) and board[x][y] == otherTile:
+                # There is a piece belonging to the other player next to our piece.
+                x += xdirection
+                y += ydirection
+                if not self.isOnBoard(x, y):
+                    continue
+                while board[x][y] == otherTile:
+                    x += xdirection
+                    y += ydirection
+                    if not self.isOnBoard(x, y): # break out of while loop, then continue in for loop
+                        break
+                if not self.isOnBoard(x, y):
+                    continue
+                if board[x][y] == tile:
+                    # There are pieces to flip over. Go in the reverse direction until we reach the original space, noting all the tiles along the way.
+                    while True:
+                        x -= xdirection
+                        y -= ydirection
+                        if x == xstart and y == ystart:
+                            break
+                        tilesToFlip.append([x, y])
+
+        board[xstart][ystart] = 0 # restore the empty space
+        if len(tilesToFlip) == 0: # If no tiles were flipped, this is not a valid move.
+            return False
+        else:
+            return tilesToFlip
+
+
+def processUserTurn(color):
+    success = False
+    try:
+        while not success: 
+            move = input('Move coords (q to quit): ')
+            if 'q' in move:
+                quit()
+            elif ',' in move:
+                int(move.split(',')[0])
+                int(move.split(',')[1])
+
+            if board.updateBoard('live',  color, int(move.split(',')[0]), int(move.split(',')[1])):
+                success = True 
+            else:
+                print('Invalid move')
+    except ValueError:
+        print("Invalid input")
+
+
+if __name__ == "__main__":
+    board = Board(10)
+    board.printBoard()
+
+    whiteWins = 0
+    blackWins = 0
+    game = 0
+
+    # Init AI player
+    player_A = Player(board, board.WHITE)
+    player_B = Player(board, board.BLACK)
+
+    iterations = 500
+    # Training - no board print
+    for i in range(iterations):
+        if board.nextTurn == board.BLACK:
+            player_B.takeTurn() 
+        else:
+            player_A.takeTurn()
+        
+        v = board.validateBoard() 
+        while not bool(v):
+            turn = board.nextTurn
+            if turn == board.BLACK:
+                player_B.takeTurn()
+            else:
+                player_A.takeTurn()
+            v = board.validateBoard()
+        if v == board.BLACK:
+            blackWins += 1
+        else:
+            whiteWins += 1
+        game += 1
+    
+        player_A.explore = 1# - game/iterations
+        player_B.explore = 1 - game/iterations 
+        board.newGame() 
+    print("White won", whiteWins, "games.")
+    print("Black won", blackWins, "games.")
+    quit()
+
+    if board.nextTurn == board.BLACK:
+        print("Black's turn")
+#        processUserTurn(board.BLACK)
+        player_B.takeTurn() 
+    else:
+        print("White's turn")
+        player_A.takeTurn()
+   
+ 
+    # Show play    
+    board.printBoard()
+
+    while board.validateBoard():
+        turn = board.nextTurn
+        if turn == board.BLACK:
+            print("Black's turn")
+#            processUserTurn(board.BLACK)
+            player_B.takeTurn()
+        else:
+            print("White's turn")
+            player_A.takeTurn()
+        board.printBoard()
---
+++ qLearn/reinforce.py
@@ -0,0 +1,152 @@
+import numpy as np
+from math import floor
+from random import random, randint
+
+
+replayMemory = list()
+
+def sig(wSum):
+    return 1/(1 + np.exp(-wSum))
+
+def dSig(a):
+    return a * (1 - a)
+
+def validateActions(board, t, color, actions):
+    validMoves = list()
+    for a in range(actions.shape[0]):
+        r = floor(a/board.size)
+        c = a % board.size
+        if board.isValidMove(t, color, r, c):
+            validMoves.append(a)
+
+    return validMoves 
+
+def adaptExperience(color, experience):
+    if experience.color != color:
+        for i in range(experience.state.shape[0]):
+            experience.state[i] *= -1
+        for i in range(experience.postState.shape[0]):
+            experience.postState[i] *= -1
+        experience.color = color
+    return experience 
+
+
+class Experience():
+    def __init__(self, color, state, action, reward, postAction):
+        self.color = color # Color of player who made the action
+        self.state = state # Board matrix flattened to a vector 
+        self.action = action # Coordinates to play 
+        self.reward = reward # Board control after action 
+        self.postState = postAction # Board state after action 
+
+class Player():
+    hSizes = [100, 100]
+    explore = 1 
+    lr = .5
+    discount = .85
+
+    def __init__(self, board, color):
+        self.color = color
+        self.board = board
+        # Get bounding sizes of weight matrices 
+        wSize = max(self.hSizes)
+        if wSize < self.board.size*self.board.size:
+            wSize = self.board.size*self.board.size 
+        self.qw = list()
+        self.b = list()   
+ 
+        # Init weights and biases 
+        for i in range(len(self.hSizes)):
+            self.b.append(np.ones(self.hSizes[i]))
+            if i == 0:
+                self.qw.append(np.random.rand(self.hSizes[i], self.board.size*self.board.size))
+            else:
+                self.qw.append(np.random.rand(self.hSizes[i], self.hSizes[i-1]))
+        self.b.append(np.ones(self.board.size*self.board.size))
+        self.qw.append(np.random.rand(self.board.size*self.board.size, self.hSizes[-1]))
+
+    def getAction(self, state, t='live'):
+        '''
+        Returns x, y, r. Coordinates and estimated reward.
+        '''
+        # Feed state forward through network
+        s = state
+        wSum = list()
+        self.a = list()
+ 
+        # Input 
+        wSum.append(np.dot(self.qw[0], s) + self.b[0])
+        self.a.append(sig(wSum[0])) 
+
+        for l in range(len(self.qw))[1:]:
+            wSum.append(np.dot(self.qw[l], self.a[-1]) + self.b[l])
+            self.a.append(sig(wSum[l]))
+
+        # Get a list of actions the player can make that are valid
+        # The actions are in the form of output indices
+        # Note that the output layer is dimensionally equivalent to a flattened board matrix
+        # So an action's coordinates can be found by division and the modulus operator
+        validActions = validateActions(self.board, t, self.color, self.a[-1])
+        if not validActions:
+            print("My turn should be skipped if I have no valid actions.") 
+            return None
+        if random() < self.explore:
+            # Random choice of action
+            act = validActions[randint(0, len(validActions)-1)]
+        else:
+            validActionActivations = list()
+            # Get activations of the valid actions
+            for i in range(len(validActions)):
+                validActionActivations.append(self.a[-1][validActions[i]])
+            aIndex = np.argmax(validActionActivations)
+            act = validActions[aIndex]
+        return (int(floor(act/self.board.size)), int(act%self.board.size)), self.a[-1][act] 
+
+    def train(self, experience):
+        # Get target reward
+        self.board.loadFromState('virtual', experience.postState)
+        if self.board.isGameOver('virtual'):
+            t = experience.reward
+        else:
+            action, reward = self.getAction(experience.postState, 'virtual')
+            # board.getOwnership(self.color) 
+            t = experience.reward + self.discount * reward 
+        self.board.syncVirtualBoard()       
+ 
+        # Backprop using an error of (t - Q(experience.state))^2
+        action, reward = self.getAction(experience.state)
+        d = list()
+        # Compute output deltas 
+        d.append((t - reward)*dSig(self.a[-1]))
+       
+        # Loop backwards through network layers to calculate all deltas 
+        for l in range(len(self.qw))[-2::-1]:
+            # d[0] is the previously added delta
+            # which is the delta for layer l+1 
+            d.insert(0, dSig(self.a[l]) * np.dot(self.qw[l+1].T, d[0]))
+
+        # Modify weights and biases starting with first layer
+        self.qw[0] += self.lr * np.dot(np.atleast_2d(d[0]).T, np.atleast_2d(experience.state))
+        self.b[0] += self.lr * d[0]
+ 
+        for l in range(len(self.qw))[1:]:
+            self.qw[l] += self.lr * np.dot(np.atleast_2d(d[l]).T, np.atleast_2d(self.a[l-1]))
+            self.b[l] += self.lr * d[l]
+        
+    def takeTurn(self):
+        state = self.board.board.flatten()
+
+        action, reward = self.getAction(state)
+
+        if not action:
+            print("Cannot move")
+        elif not self.board.updateBoard('live', self.color, action[0], action[1]):
+            print("FAILED MOVE")
+
+            experience = Experience(self.color, state, action, reward, self.board.board.flatten())
+            replayMemory.append(experience)
+        
+        # Train on experience sampled from replay memory
+        if len(replayMemory) > 0:
+            sampExp = adaptExperience(self.color, replayMemory[randint(0, len(replayMemory)-1)])
+            self.train(sampExp)

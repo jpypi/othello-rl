63a4dbe49b3717e1a6c0dc6d63122c43b677e3ca
James Jenkins <applesmacs@yahoo.com>
2016-06-25 13:55:10 -0400

    Implement Normal Q learning... I think.

--- players.py
+++ players.py
@@ -7,11 +7,15 @@ import nn
 
 
 class RLPlayer:
-    def __init__(self):
+    def __init__(self, q_lr, discount_factor, net_lr = 0.01):
         # We ougth to use softmax in this
-        self.policy_net = nn.NN([64, 128, 64], 0.02)
+        self.policy_net = nn.NN([64, 128, 128, 64, 64], net_lr)
         # This ought to decay
         self.epsilon = 0.6
+
+        # Variables for Q learning discount reward
+        self.q_lr = q_lr
+        self.discount_factor = discount_factor
 
         self.prepForNewGame()
 
@@ -24,8 +28,6 @@ class RLPlayer:
         # to -1s
         input_state = np.apply_along_axis(lambda x: int((x==me and 1) or (x!=0 and -1)),
                                           1, board_state.reshape((64, 1))).reshape((64,1))
-        out = self.policy_net.getOutput(input_state)
-
         made_move = False
         pos = None
 
@@ -42,6 +44,7 @@ class RLPlayer:
                 return False
 
         else:
+            out = self.policy_net.getOutput(input_state)
             # Sort the possible moves lowest to highest desire
             positions = [(v,i) for i,v in enumerate(out)]
             positions.sort(key = lambda x: x[0], reverse = True)
@@ -65,8 +68,31 @@ class RLPlayer:
         return True
 
     def updateWeights(self, final_score):
-        for i, (state, output) in enumerate(self.play_history):
-            self.policy_net.backProp(state, final_score * OneHot(output, 64))
+        #for i, (state, action) in enumerate(self.play_history):
+        #    self.policy_net.backProp(state, final_score * OneHot(action, 64))
+
+        i = 0
+        state, action = self.play_history[i]
+        q = self.policy_net.getOutput(state)
+        targets = []
+        while i+1 < len(self.play_history):
+            i += 1
+            state_, action_ = self.play_history[i]
+            q_ = self.policy_net.getOutput(state_)
+
+            r = final_score if i+1 == len(self.play_history) else 0
+            target = np.zeros(64)
+            target[action] = q[action] + self.q_lr * (r + self.discount_factor * np.max(q_) - q[action])
+            targets.append(target)
+            action, q = action_, q_
+
+        target = np.zeros(64)
+        target[self.play_history[-1][1]] = final_score
+        targets.append(target)
+
+        for i in range(len(play_history)-1, -1, -1)
+            t = self.policy_net.mkVec(targets[i])
+            self.policy_net.backProp(state, t)
 
 
 def OneHot(index, dim):
